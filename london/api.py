123fde2e13dcb2e22b873657d9e5c305ae35c37d
Thomas Castiglione <castiglione@gmail.com>
2013-06-23 02:22:57 +0800

    Parse output and display results

--- london/api.py
+++ london/api.py
@@ -1,28 +1,42 @@
+import bs4
 from . import site
 
 def travel(area):
-    print('Travelling to {0}'.format(area.__name__))
     site.command('/Map/Move', {'areaid': area._id})
+    print('Welcome to {0}, delicious friend!'.format(area.__name__))
+
 
 def use_item(item):
-    print('Using {0}'.format(item.__name__))
-    site.command('/Storylet/UseQuality', {'qualityId': item._id})
+    html = site.command('/Storylet/UseQuality', {'qualityId': item._id})
+    soup = bs4.BeautifulSoup(html)
+    print('Used {0}'.format(item.__name__))
 
     if hasattr(item, 'default'):
         global default_branch
         default_branch = item.default
 
+
 def begin_story(storylet):
-    print('Beginning {0}'.format(storylet.__name__))
-    site.command('/Storylet/Begin', {'eventid': storylet._id})
+    html = site.command('/Storylet/Begin', {'eventid': storylet._id})
+    soup = bs4.BeautifulSoup(html)
+    print('"{0}"'.format(soup.h3.string.strip()))
 
     if hasattr(storylet, 'default'):
         global default_branch
         default_branch = storylet.default
 
+
+def choose_branch(branch):
+    html = site.command('/Storylet/ChooseBranch', {'branchid': branch})
+    soup = bs4.BeautifulSoup(html)
+    effects = soup.find_all('p')
+
+    for tag in effects[1:]:
+        content = ''.join(tag.strings)
+        if not 'You succeeded' in content:
+            print(content)
+
+
 def choose_default():
     global default_branch
     choose_branch(default_branch)
-
-def choose_branch(branch):
-    site.command('/Storylet/ChooseBranch', {'branchid': branch})
--- london/site.py
+++ london/site.py
@@ -11,23 +11,23 @@ def command(page, data):
     #uri = BASE_URI + page
     #inner = urllib.parse.urlencode(data)
     #get('/Gap/Load', {'content': page + '?' + inner})
-    post(page, data)
+    return post(page, data)
 
 def get(uri, query):
     data = urllib.parse.urlencode(query)
     uri = uri + '?' + data
-    print(uri)
-    page = client.open(uri)
-    if page.getcode() != 200:
-        raise Exception('Request failed')
+    with client.open(uri) as page:
+        if page.getcode() != 200:
+            raise Exception('Request failed: {0} {1}'.format(uri, query))
+        return page.read().decode('utf-8')
 
 def post(page, query):
     uri = BASE_URI + page
-    print(uri)
     data = urllib.parse.urlencode(query).encode('utf-8')
-    page = client.open(uri, data)
-    if page.getcode() != 200:
-        raise Exception('Request failed')
+    with client.open(uri, data) as page:
+        if page.getcode() != 200:
+            raise Exception('Request failed: {0} {1}'.format(uri, query))
+        return page.read().decode('utf-8')
 
 print('Entering the Neath')
 post('/Auth/EmailLogin', {'emailAddress': _settings.AUTH_USER, 'password': _settings.AUTH_PASS})
