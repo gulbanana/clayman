fb7caa34aeb188d5d9ca26c76bb59f2cca4d1784
Thomas Castiglione <castiglione@gmail.com>
2013-06-23 22:51:02 +0800

    more samples

--- jobs/grind.py
+++ jobs/grind.py
@@ -2,10 +2,34 @@ import _settings
 from london import *
 from .common import *
 
+def grind_wines(character):
+    """+37 Greyfields 1882 per action (0.74E)"""
+    character.travel(areas.SHUTTERED_PALACE)
+    character.begin_story('The Antiquarian Footman')
+    character.choose_branch('Offer the fellow a partnership')
+    character.onwards()
+
+def grind_secrets(character):
+    """+54 whispered secrets per action (0.54E)"""
+    character.travel(areas.VEILGARDEN)
+    character.begin_story('The demands of high society')
+    character.choose_branch('Attend an event')
+    character.onwards()
+
+def grind_clues(character):
+    """+30 cryptic clues per action(0.60E)"""
+    character.travel(areas.VEILGARDEN)
+    character.begin_story('Correspond with a contact at the Shuttered Palace')
+    character.choose_branch('Write a letter')
+    character.onwards()
+
 character = Gentleperson(_settings.AUTH_USER, _settings.AUTH_PASS)
 
-buffer = character.action_cap - 2
-
-print('actions before: ' +str(character.actions))
-grind_wines(character)
-print('actions after: ' +str(character.actions))
+buffer = character.action_cap - 8   # script is only called once per hour
+while character.actions > buffer:
+    if character.menaces['Scandal'] < 7:
+        grind_wines(character)
+    elif character.items['Whispered Secret'] < 80000:
+        grind_secrets(character)
+    else:
+        grind_clues(character)
