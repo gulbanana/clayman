92855a4d8f2450ee71c3940817ffe9340a48f18a
Thomas Castiglione <castiglione@gmail.com>
2013-06-25 01:13:23 +0800

    reenable wines grind

--- jobs/grind.py
+++ jobs/grind.py
@@ -6,10 +6,9 @@ character = Character(_settings.AUTH_USER, _settings.AUTH_PASS)
 buffer = character.action_cap - 4
 
 while character.actions > buffer:
-    #if character.menaces['Scandal'] < 7 and character.items['Greyfields 1882'] < 1000:
-    #    grind_wines(character)
-    #elif character.items['Whispered Secret'] < 80000:
-    if character.items['Whispered Secret'] < 80000:
+    if character.menaces['Scandal'] < 7 and character.items['Greyfields 1882'] < 1000:
+        grind_wines(character)
+    elif character.items['Whispered Secret'] < 80000:
         grind_secrets(character)
     elif character.menaces['Scandal'] < 7:
         grind_jade(character)
