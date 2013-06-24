9540db8a890e3fa96cd7a9440537381683b8d93f
Thomas Castiglione <castiglione@gmail.com>
2013-06-24 12:46:08 +0800

    new grind options

--- jobs/common.py
+++ jobs/common.py
@@ -1,2 +1,40 @@
 from london import *
 
+def grind_jade(character):
+    """+37 Greyfields 1882 per action (0.74E)
+    scandal on failure
+    max persuasive 84 (72% chance)"""
+    character.travel(areas.SHUTTERED_PALACE)
+    character.begin_story('Become a trader in antiques')
+    character.choose_branch('Deal in pretty trinkets')
+    character.onwards()
+
+def grind_wines(character):
+    """+37 Greyfields 1882 per action (0.74E)
+    scandal on failure
+    max persuasive 84 (72% chance)"""
+    character.travel(areas.SHUTTERED_PALACE)
+    character.begin_story('The Antiquarian Footman')
+    character.choose_branch('Offer the fellow a partnership')
+    character.onwards()
+
+def grind_secrets(character):
+    """+54 whispered secrets per action (0.54E)"""
+    character.travel(areas.VEILGARDEN)
+    character.begin_story('The demands of high society')
+    character.choose_default_branch()
+    character.onwards()
+
+def grind_clues(character):
+    """+30 cryptic clues per action(0.60E)"""
+    character.travel(areas.VEILGARDEN)
+    character.begin_story('Correspond with a contact at the Shuttered Palace')
+    character.choose_branch('Write a letter')
+    character.onwards()
+
+def grind_souls(character):
+    """+28 soul (0.56E)
+    also has a Shadowy option"""
+    character.travel(areas.LADYBONES_ROAD)
+    character.begin_story('Track down a Spirifer')
+    character.choose_branch('Watch from the rooftops')
--- jobs/grind.py
+++ jobs/grind.py
@@ -2,35 +2,15 @@ import _settings
 from london import *
 from .common import *
 
-def grind_wines(character):
-    """+37 Greyfields 1882 per action (0.74E)
-    max persuasive 84 (72% chance)"""
-    character.travel(areas.SHUTTERED_PALACE)
-    character.begin_story('The Antiquarian Footman')
-    character.choose_branch('Offer the fellow a partnership')
-    character.onwards()
-
-def grind_secrets(character):
-    """+54 whispered secrets per action (0.54E)"""
-    character.travel(areas.VEILGARDEN)
-    character.begin_story('The demands of high society')
-    character.choose_branch('Attend an event')
-    character.onwards()
-
-def grind_clues(character):
-    """+30 cryptic clues per action(0.60E)"""
-    character.travel(areas.VEILGARDEN)
-    character.begin_story('Correspond with a contact at the Shuttered Palace')
-    character.choose_branch('Write a letter')
-    character.onwards()
-
 character = Character(_settings.AUTH_USER, _settings.AUTH_PASS)
 
 buffer = character.action_cap - 4
 while character.actions > buffer:
-    if character.menaces['Scandal'] < 7:
+    if character.menaces['Scandal'] < 7 and character.items['Greyfields 1882'] < 1000:
         grind_wines(character)
     elif character.items['Whispered Secret'] < 80000:
         grind_secrets(character)
+    elif character.menaces['Scandal'] < 7:
+        grind_jade(character)
     else:
         grind_clues(character)
