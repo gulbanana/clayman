5bb23884310ce4cd6bac7c974f10b7ee158ffd24
Thomas Castiglione <castiglione@gmail.com>
2013-06-24 13:10:11 +0800

    ordered some options by EV

--- jobs/common.py
+++ jobs/common.py
@@ -1,16 +1,15 @@
 from london import *
 
 def grind_jade(character):
-    """+37 Greyfields 1882 per action (0.74E)
-    scandal on failure
-    max persuasive 84 (72% chance)"""
+    """+75 jade (0.75E)
+    scandal on failure"""
     character.travel(areas.SHUTTERED_PALACE)
     character.begin_story('Become a trader in antiques')
     character.choose_branch('Deal in pretty trinkets')
     character.onwards()
 
 def grind_wines(character):
-    """+37 Greyfields 1882 per action (0.74E)
+    """+37 Greyfields 1882 (0.74E)
     scandal on failure
     max persuasive 84 (72% chance)"""
     character.travel(areas.SHUTTERED_PALACE)
@@ -18,15 +17,8 @@ def grind_wines(character):
     character.choose_branch('Offer the fellow a partnership')
     character.onwards()
 
-def grind_secrets(character):
-    """+54 whispered secrets per action (0.54E)"""
-    character.travel(areas.VEILGARDEN)
-    character.begin_story('The demands of high society')
-    character.choose_default_branch()
-    character.onwards()
-
 def grind_clues(character):
-    """+30 cryptic clues per action(0.60E)"""
+    """+30 cryptic clues (0.60E)"""
     character.travel(areas.VEILGARDEN)
     character.begin_story('Correspond with a contact at the Shuttered Palace')
     character.choose_branch('Write a letter')
@@ -38,3 +30,10 @@ def grind_souls(character):
     character.travel(areas.LADYBONES_ROAD)
     character.begin_story('Track down a Spirifer')
     character.choose_branch('Watch from the rooftops')
+
+def grind_secrets(character):
+    """+54 whispered secrets per action (0.54E)"""
+    character.travel(areas.VEILGARDEN)
+    character.begin_story('The demands of high society')
+    character.choose_default_branch()
+    character.onwards()
--- jobs/grind.py
+++ jobs/grind.py
@@ -3,8 +3,8 @@ from london import *
 from .common import *
 
 character = Character(_settings.AUTH_USER, _settings.AUTH_PASS)
-
 buffer = character.action_cap - 4
+
 while character.actions > buffer:
     if character.menaces['Scandal'] < 7 and character.items['Greyfields 1882'] < 1000:
         grind_wines(character)
